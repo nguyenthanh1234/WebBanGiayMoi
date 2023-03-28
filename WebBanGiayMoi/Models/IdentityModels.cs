b1c89c7a65d756de7064ab6e5e64ddb715607f91
Nguyễn Văn Thanh <nguyenzvanzthanh@gmail.com>
2023-03-28 22:08:41 +0700

    Thêm câu hello

--- WebBanGiayMoi/Models/IdentityModels.cs
+++ WebBanGiayMoi/Models/IdentityModels.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace WebBanGiayMoi.Models
 {
+
+    //Hello
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
