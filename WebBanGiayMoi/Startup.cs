13ecb6cfc0431605497dbd1c8d878a1751d6374d
Nguyễn Văn Thanh <nguyenzvanzthanh@gmail.com>
2023-03-28 22:05:51 +0700

    Add project files.

---
+++ WebBanGiayMoi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebBanGiayMoi.Startup))]
+namespace WebBanGiayMoi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
