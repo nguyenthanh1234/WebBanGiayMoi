ed0c5fa95ef07f01255aaabc4a7d639e270bd4bf
Nguyễn Văn Thanh <nguyenzvanzthanh@gmail.com>
2023-05-17 18:41:38 +0700

    Cập nhập xong

--- WebBanGiayMoi/Controllers/ShoppingCartController.cs
+++ WebBanGiayMoi/Controllers/ShoppingCartController.cs
@@ -2,6 +2,7 @@
 using PayPal.Api;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Drawing;
 using System.Linq;
 using System.Security.AccessControl;
@@ -10,6 +11,7 @@ using System.Web.Mvc;
 using System.Web.UI.WebControls;
 using WebBanGiayMoi.Helper;
 using WebBanGiayMoi.Models;
+using WebBanGiayMoi.Models.VNPAY;
 
 namespace WebBanGiayMoi.Controllers
 {
@@ -328,6 +330,127 @@ namespace WebBanGiayMoi.Controllers
             };
             // Create a payment using a APIContext  
             return this.payment.Create(apiContext);
+        }
+
+
+        //VNPAY
+        public ActionResult Payment()
+        {
+            string url = ConfigurationManager.AppSettings["Url"];
+            string returnUrl = ConfigurationManager.AppSettings["ReturnUrl"];
+            string tmnCode = ConfigurationManager.AppSettings["TmnCode"];
+            string hashSecret = ConfigurationManager.AppSettings["HashSecret"];
+
+            PayLib pay = new PayLib();
+            Cart cart = Session["Cart"] as Cart;
+
+
+            pay.AddRequestData("vnp_Version", "2.1.0"); //Phiên bản api mà merchant kết nối. Phiên bản hiện tại là 2.1.0
+            pay.AddRequestData("vnp_Command", "pay"); //Mã API sử dụng, mã cho giao dịch thanh toán là 'pay'
+            pay.AddRequestData("vnp_TmnCode", tmnCode); //Mã website của merchant trên hệ thống của VNPAY (khi đăng ký tài khoản sẽ có trong mail VNPAY gửi về)
+            pay.AddRequestData("vnp_Amount", (cart.Total_Money() * 100).ToString()); //số tiền cần thanh toán, công thức: số tiền * 100 - ví dụ 10.000 (mười nghìn đồng) --> 1000000
+            pay.AddRequestData("vnp_BankCode", ""); //Mã Ngân hàng thanh toán (tham khảo: https://sandbox.vnpayment.vn/apis/danh-sach-ngan-hang/), có thể để trống, người dùng có thể chọn trên cổng thanh toán VNPAY
+            pay.AddRequestData("vnp_CreateDate", DateTime.Now.ToString("yyyyMMddHHmmss")); //ngày thanh toán theo định dạng yyyyMMddHHmmss
+            pay.AddRequestData("vnp_CurrCode", "VND"); //Đơn vị tiền tệ sử dụng thanh toán. Hiện tại chỉ hỗ trợ VND
+            pay.AddRequestData("vnp_IpAddr", Util.GetIpAddress()); //Địa chỉ IP của khách hàng thực hiện giao dịch
+            pay.AddRequestData("vnp_Locale", "vn"); //Ngôn ngữ giao diện hiển thị - Tiếng Việt (vn), Tiếng Anh (en)
+            pay.AddRequestData("vnp_OrderInfo", "Thanh toan don hang"); //Thông tin mô tả nội dung thanh toán
+            pay.AddRequestData("vnp_OrderType", "other"); //topup: Nạp tiền điện thoại - billpayment: Thanh toán hóa đơn - fashion: Thời trang - other: Thanh toán trực tuyến
+            pay.AddRequestData("vnp_ReturnUrl", returnUrl); //URL thông báo kết quả giao dịch khi Khách hàng kết thúc thanh toán
+            pay.AddRequestData("vnp_TxnRef", DateTime.Now.Ticks.ToString()); //mã hóa đơn
+
+            string paymentUrl = pay.CreateRequestUrl(url, hashSecret);
+
+            return Redirect(paymentUrl);
+        }
+
+
+        public ActionResult PaymentConfirm(FormCollection form)
+        {
+            if (Request.QueryString.Count > 0)
+            {
+                string hashSecret = ConfigurationManager.AppSettings["HashSecret"]; //Chuỗi bí mật
+                var vnpayData = Request.QueryString;
+                PayLib pay = new PayLib();
+
+                //lấy toàn bộ dữ liệu được trả về
+                foreach (string s in vnpayData)
+                {
+                    if (!string.IsNullOrEmpty(s) && s.StartsWith("vnp_"))
+                    {
+                        pay.AddResponseData(s, vnpayData[s]);
+                    }
+                }
+
+                long orderId = Convert.ToInt64(pay.GetResponseData("vnp_TxnRef")); //mã hóa đơn
+                long vnpayTranId = Convert.ToInt64(pay.GetResponseData("vnp_TransactionNo")); //mã giao dịch tại hệ thống VNPAY
+                string vnp_ResponseCode = pay.GetResponseData("vnp_ResponseCode"); //response code: 00 - thành công, khác 00 - xem thêm https://sandbox.vnpayment.vn/apis/docs/bang-ma-loi/
+                string vnp_SecureHash = Request.QueryString["vnp_SecureHash"]; //hash của dữ liệu trả về
+
+                bool checkSignature = pay.ValidateSignature(vnp_SecureHash, hashSecret); //check chữ ký đúng hay không?
+
+                if (checkSignature)
+                {
+                    if (vnp_ResponseCode == "00")
+                    {
+                        //Thanh toán thành công
+                        ViewBag.Message = "Thanh toán thành công hóa đơn " + orderId + " | Mã giao dịch: " + vnpayTranId;
+                        try
+                        {
+                            Cart cart = Session["Cart"] as Cart;
+                            if (cart.Items.Count() == 0)
+                            {
+                                return RedirectToAction("index", "home");
+                            }
+                            Models.Order _order = new Models.Order();
+                            _order.OrderDate = DateTime.Now;
+                            if (Session["Profile"] == null)
+                            {
+                                return RedirectToAction("login", "Account");
+                            }
+                            var userCurrent = Session["Profile"] as ApplicationUser;
+                            _order.ApplicationUserId = userCurrent.Id;
+                            _order.Descriptions = userCurrent.Address;
+                            _order.PhoneKH = userCurrent.Phone;
+                            _order.PhuongThucTT = "VNPAY";
+
+                            _db.Orders.Add(_order);
+                            foreach (var item in cart.Items)
+                            {
+                                OrderDetail _order_Detail = new OrderDetail();
+                                _order_Detail.OrderId = _order.Id;
+                                _order_Detail.GiayId = item._shopping_product.Id;
+                                _order_Detail.UnitPriceSale = item._shopping_product.Gia;
+                                _order_Detail.QuantitySale = item._shopping_quantity;
+                                _order_Detail.Size = item._shopping_size;
+                                _db.OrderDetails.Add(_order_Detail);
+                            }
+                            _db.SaveChanges();
+                            var gioHangCuaToi = Session["Cart"] as Cart;
+                            SendMail.SendEmail(/*form["Email"]*/ userCurrent.Email, "Cảm ơn bạn đã mua hàng tại website giày pro",
+                           "Tổng số tiền là: " + gioHangCuaToi.Total_Money() + " - Chúc bạn mua sắm vui vẻ!!!", "");
+                            cart.ClearCart();
+
+                            return RedirectToAction("Shopping_Success", "ShoppingCart");
+                        }
+                        catch
+                        {
+                            return Content("Đã xảy ra lỗi, mời bạn kiểm tra lại");
+                        }
+                    }
+                    else
+                    {
+                        //Thanh toán không thành công. Mã lỗi: vnp_ResponseCode
+                        ViewBag.Message = "Có lỗi xảy ra trong quá trình xử lý hóa đơn " + orderId + " | Mã giao dịch: " + vnpayTranId + " | Mã lỗi: " + vnp_ResponseCode;
+                    }
+                }
+                else
+                {
+                    ViewBag.Message = "Có lỗi xảy ra trong quá trình xử lý";
+                }
+            }
+
+            return View("Shopping_Failure");
         }
     }
 }
