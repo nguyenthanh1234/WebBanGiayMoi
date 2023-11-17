7114cca7fdaf1a5e83ff27ce6634314ab7fc225d
Nguyễn Văn Thanh <nguyenzvanzthanh@gmail.com>
2023-11-17 13:27:11 +0700

    cập nhập ngày 17/11

--- WebBanGiayMoi/Models/Topic.cs
+++ WebBanGiayMoi/Models/Topic.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
@@ -10,6 +12,7 @@ namespace WebBanGiayMoi.Models
     public class Topic
     {
         public int Id { get; set; }
+        [Display(Name ="Chủ đề")]
         public string NameTopic { get; set; }
 
     }
